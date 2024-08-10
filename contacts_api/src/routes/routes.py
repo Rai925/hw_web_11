1edc3d78f9133bc96ab8c85ef5f91542e62aae95
Rai925 <154880397+Rai925@users.noreply.github.com>
2024-08-10 07:10:58 +0300

    Update routes.py

--- contacts_api/src/routes/routes.py
+++ contacts_api/src/routes/routes.py
@@ -43,6 +43,16 @@ def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
     return contacts
 
 
+@router.get("/contacts/search/", response_model=List[ContactResponse])
+def search_contacts_route(name: Optional[str] = None, email: Optional[str] = None, db: Session = Depends(get_db)):
+    return search_contacts(db, name, email)
+
+
+@router.get("/contacts/birthday/", response_model=List[ContactResponse])
+def contacts_birthday_soon(days: int = 7, db: Session = Depends(get_db)):
+    return get_contacts_birthday_soon(db, days)
+
+
 @router.get("/contacts/{contact_id}", response_model=ContactResponse)
 def read_contact(contact_id: int, db: Session = Depends(get_db)):
     db_contact = get_contact(db, contact_id)
@@ -67,11 +77,4 @@ def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     return db_contact
 
 
-@router.get("/contacts/search/", response_model=List[ContactResponse])
-def search_contacts_route(name: Optional[str] = None, email: Optional[str] = None, db: Session = Depends(get_db)):
-    return search_contacts(db, name, email)
 
-
-@router.get("/contacts/birthday/", response_model=List[ContactResponse])
-def contacts_birthday_soon(days: int = 7, db: Session = Depends(get_db)):
-    return get_contacts_birthday_soon(db, days)
